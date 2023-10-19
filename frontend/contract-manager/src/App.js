a79bf9802b1ed16866d3abc09532b17c8eb45a2a
MalekeHan <Malekehanson@gmail.com>
2023-10-19 02:31:18 -0700

    Moved FrontEnd Code Here

---
+++ frontend/contract-manager/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from './components/HomePage';
+import NavBar from './components/Navbar';
+import Footer from './components/Footer';
+import SignIn from "./components/SignIn";
+import Register from "./components/Register";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <NavBar />
+          <Routes>
+            <Route
+              path="/"
+              element={<Homepage />}
+            />
+            <Route
+              path="/SignIn"
+              element={<SignIn />}
+            />
+            <Route
+              path="/Register"
+              element={<Register />}
+            />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
