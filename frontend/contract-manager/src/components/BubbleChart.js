4fbca41d8c32fe8f4a5b621436af0d75560f856c
MalekeHan <malekehanson@gmail.com>
2023-12-01 11:01:39 -0800

    Better viz

--- frontend/contract-manager/src/components/BubbleChart.js
+++ frontend/contract-manager/src/components/BubbleChart.js
@@ -1,81 +1,38 @@
 import React, { useEffect, useState, useRef } from "react";
 import * as d3 from "d3";
-import axios from 'axios'; // Ensure axios is installed
+import axios from 'axios';
 
 const BubbleChart = () => {
   const [nodes, setNodes] = useState([]);
-  const svgRef = useRef(null);
+  const svgRef = useRef();
 
   const fetchContractData = async () => {
     try {
       const contractsResponse = await axios.get('https://contract-manager.aquaflare.io/contracts/');
       const partnersResponse = await axios.get('https://contract-manager.aquaflare.io/partners/');
-
+      
+      // Calculate the total amount paid in all contracts
       const totalAmount = contractsResponse.data.reduce((sum, contract) => sum + contract.amount_paid, 0);
-      const partnerAmounts = partnersResponse.data.map((partner, index) => ({
-        id: partner.id,
-        name: partner.name,
-        totalAmount: contractsResponse.data
+      
+      // Map the partnersResponse to a new array that includes a totalAmount per partner
+      const partnerAmounts = partnersResponse.data.map(partner => {
+        const totalAmountForPartner = contractsResponse.data
           .filter(contract => contract.partner === partner.id)
-          .reduce((sum, contract) => sum + contract.amount_paid, 0),
-        category: index % 3 
-      }));
+          .reduce((sum, contract) => sum + contract.amount_paid, 0);
 
-      const nodeData = partnerAmounts.map(partner => ({
-        radius: (partner.totalAmount / totalAmount) * 100, // make the size bigger based on the percentage of contracts
-        category: partner.category,
-        name: partner.name
-      }));
+        return {
+          id: partner.id,
+          name: partner.name,
+          totalAmount: totalAmountForPartner,
+          group: partner.id, // Assuming each partner is a separate group
+          radius: (totalAmountForPartner / totalAmount) * 100 // Radius proportional to the percentage of total amount
+        };
+      });
 
-      setNodes(nodeData);
+      setNodes(partnerAmounts); // Set the nodes for the D3 chart
     } catch (error) {
       console.error("Error fetching contract data:", error);
     }
-  };
-
-  const drawChart = () => {
-    const svg = d3.select(svgRef.current)
-      .attr('width', 600)
-      .attr('height', 400)
-      .attr("viewBox", `0 0 600 400`); // Adjust viewBox if needed
-
-    var colorScale = ['orange', 'lightblue', '#B19CD9'];
-    var xCenter = [100, 300, 500];
-
-    var simulation = d3.forceSimulation(nodes)
-      .force('charge', d3.forceManyBody().strength(5))
-      .force('x', d3.forceX().x(function(d) {
-        return xCenter[d.category];
-      }))
-      .force('collision', d3.forceCollide().radius(function(d) {
-        return d.radius;
-      }))
-      .on('tick', ticked);
-
-    function ticked() {
-      var u = svg.selectAll('circle')
-        .data(nodes)
-        .join('circle')
-        .attr('r', function(d) {
-          return d.radius;
-        })
-        .style('fill', function(d) {
-          return colorScale[d.category];
-        })
-        .attr('cx', function(d) {
-          return d.x;
-        })
-        .attr('cy', function(d) {
-          return d.y;
-        });
-
-        u.each(function(d) {
-          console.log(`Node: ${d.name}, x: ${d.x}, y: ${d.y}`);
-        });
-
-    }
-
-    simulation.nodes(nodes).on('tick', ticked);
   };
 
   useEffect(() => {
@@ -83,8 +40,59 @@ const BubbleChart = () => {
   }, []);
 
   useEffect(() => {
-    drawChart();
+    if (nodes.length > 0) {
+      drawChart();
+    }
   }, [nodes]);
+
+  const drawChart = () => {
+    const svg = d3.select(svgRef.current)
+      .attr('width', width)
+      .attr('height', height);
+
+    // A color scale: one color for each group
+    const color = d3.scaleOrdinal()
+      .domain(nodes.map(d => d.group))
+      .range(d3.schemeCategory10);
+
+    // A scale that gives a X target position for each group
+    const x = d3.scaleOrdinal()
+      .domain(nodes.map(d => d.group))
+      .range([50, width / 2, width - 50]);
+
+    // Initialize the circles: all located at the center of the svg area
+    const node = svg.selectAll("circle")
+      .data(nodes)
+      .enter()
+      .append("circle")
+        .attr("r", d => d.radius)
+        .attr("cx", width / 2)
+        .attr("cy", height / 2)
+        .style("fill", d => color(d.group))
+        .style("fill-opacity", 0.8)
+        .attr("stroke", "black")
+        .style("stroke-width", 4);
+
+    // Features of the forces applied to the nodes:
+    const simulation = d3.forceSimulation()
+        .force("x", d3.forceX().strength(0.5).x(d => x(d.group)))
+        .force("y", d3.forceY().strength(0.1).y(height / 2))
+        .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
+        .force("charge", d3.forceManyBody().strength(1)) // Nodes are attracted one each other of value is > 0
+        .force("collide", d3.forceCollide().strength(.1).radius(d => d.radius + 2).iterations(1)); // Force that avoids circle overlapping
+
+    simulation
+      .nodes(nodes)
+      .on("tick", () => {
+        node
+          .attr("cx", d => d.x)
+          .attr("cy", d => d.y);
+      });
+  };
+
+  // Set the dimensions of the graph
+  const width = 450;
+  const height = 450;
 
   return <svg ref={svgRef}></svg>;
 };
