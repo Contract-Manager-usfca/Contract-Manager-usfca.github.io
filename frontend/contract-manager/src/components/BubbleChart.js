4b0bb71b5ee4b204665021e7a344a56b3e454a75
MalekeHan <malekehanson@gmail.com>
2023-12-01 11:29:19 -0800

    Bubbles are off the chart (lolz)

--- frontend/contract-manager/src/components/BubbleChart.js
+++ frontend/contract-manager/src/components/BubbleChart.js
@@ -4,32 +4,37 @@ import axios from 'axios';
 
 const BubbleChart = () => {
   const [nodes, setNodes] = useState([]);
-  const svgRef = useRef();
+  const svgRef = useRef(null);
+  const simulationRef = useRef(null);
 
   const fetchContractData = async () => {
     try {
       const contractsResponse = await axios.get('https://contract-manager.aquaflare.io/contracts/');
       const partnersResponse = await axios.get('https://contract-manager.aquaflare.io/partners/');
-      
-      // Calculate the total amount paid in all contracts
-      const totalAmount = contractsResponse.data.reduce((sum, contract) => sum + contract.amount_paid, 0);
-      
-      // Map the partnersResponse to a new array that includes a totalAmount per partner
-      const partnerAmounts = partnersResponse.data.map(partner => {
-        const totalAmountForPartner = contractsResponse.data
-          .filter(contract => contract.partner === partner.id)
-          .reduce((sum, contract) => sum + contract.amount_paid, 0);
 
+      const totalAmount = contractsResponse.data.reduce((sum, contract) => sum + contract.amount_paid, 0);
+      const partnerAmounts = partnersResponse.data.map(partner => ({
+        id: partner.id,
+        name: partner.name,
+        totalAmount: contractsResponse.data
+          .filter(contract => contract.partner === partner.id)
+          .reduce((sum, contract) => sum + contract.amount_paid, 0),
+        category: partner.id, // Assuming each partner is a separate category
+      }));
+
+      // Create a node for each contract
+      const contractNodes = contractsResponse.data.map(contract => {
+        const partner = partnerAmounts.find(p => p.id === contract.partner);
         return {
-          id: partner.id,
-          name: partner.name,
-          totalAmount: totalAmountForPartner,
-          group: partner.id, // Assuming each partner is a separate group
-          radius: (totalAmountForPartner / totalAmount) * 100 // Radius proportional to the percentage of total amount
+          id: contract.id,
+          partnerName: partner ? partner.name : 'Unknown',
+          amount_paid: contract.amount_paid,
+          group: contract.partner,
+          radius: 20, // You can also scale this based on the contract amount if desired
         };
       });
 
-      setNodes(partnerAmounts); // Set the nodes for the D3 chart
+      setNodes(contractNodes);
     } catch (error) {
       console.error("Error fetching contract data:", error);
     }
@@ -40,59 +45,79 @@ const BubbleChart = () => {
   }, []);
 
   useEffect(() => {
-    if (nodes.length > 0) {
+    if (nodes.length > 0 && svgRef.current) {
       drawChart();
     }
   }, [nodes]);
 
   const drawChart = () => {
+    const width = 600; // Width of the SVG container
+    const height = 600; // Height of the SVG container
+    const padding = 50; // Padding to ensure bubbles don't touch the SVG edges
+
     const svg = d3.select(svgRef.current)
       .attr('width', width)
       .attr('height', height);
 
-    // A color scale: one color for each group
     const color = d3.scaleOrdinal()
       .domain(nodes.map(d => d.group))
       .range(d3.schemeCategory10);
 
-    // A scale that gives a X target position for each group
     const x = d3.scaleOrdinal()
       .domain(nodes.map(d => d.group))
       .range([50, width / 2, width - 50]);
 
-    // Initialize the circles: all located at the center of the svg area
-    const node = svg.selectAll("circle")
-      .data(nodes)
-      .enter()
-      .append("circle")
+    const node = svg.append("g")
+      .selectAll("circle")
+      .data(nodes, d => d.id) // key function to identify each node
+      .join("circle")
         .attr("r", d => d.radius)
-        .attr("cx", width / 2)
-        .attr("cy", height / 2)
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y)
         .style("fill", d => color(d.group))
         .style("fill-opacity", 0.8)
         .attr("stroke", "black")
-        .style("stroke-width", 4);
+        .style("stroke-width", 4)
+        .call(d3.drag()
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended));
 
-    // Features of the forces applied to the nodes:
-    const simulation = d3.forceSimulation()
+    simulationRef.current = d3.forceSimulation()
         .force("x", d3.forceX().strength(0.5).x(d => x(d.group)))
         .force("y", d3.forceY().strength(0.1).y(height / 2))
-        .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
-        .force("charge", d3.forceManyBody().strength(1)) // Nodes are attracted one each other of value is > 0
-        .force("collide", d3.forceCollide().strength(.1).radius(d => d.radius + 2).iterations(1)); // Force that avoids circle overlapping
-
-    simulation
-      .nodes(nodes)
-      .on("tick", () => {
-        node
-          .attr("cx", d => d.x)
-          .attr("cy", d => d.y);
-      });
+        .force("center", d3.forceCenter().x(width / 2).y(height / 2))
+        .force("charge", d3.forceManyBody().strength(1))
+        .force("collide", d3.forceCollide().strength(.1).radius(d => d.radius + 2).iterations(1))
+        .nodes(nodes)
+        .on("tick", () => {
+          node
+            .attr("cx", d => d.x)
+            .attr("cy", d => d.y);
+        });
   };
 
-  // Set the dimensions of the graph
-  const width = 450;
-  const height = 450;
+  // Drag event handlers
+  const dragstarted = (event, d) => {
+    if (!event.active) simulationRef.current.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  };
+
+  const dragged = (event, d) => {
+    d.fx = event.x;
+    d.fy = event.y;
+  };
+
+  const dragended = (event, d) => {
+    if (!event.active) simulationRef.current.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+    // Restart the simulation with a low alpha so that it cools down slowly
+    simulationRef.current.alpha(0.1).restart();
+  };
+
+  
 
   return <svg ref={svgRef}></svg>;
 };
